7f3b60a338ae2c0731da7215a6155f0f7c9a02bf
Alexander Steen <a.steen@fu-berlin.de>
2021-11-29 21:15:43 +0100

    more on reparse

--- tptp-utils-runtime/src/main/scala/leo/modules/tptputils/ParseTree.scala
+++ tptp-utils-runtime/src/main/scala/leo/modules/tptputils/ParseTree.scala
@@ -1,6 +1,7 @@
 package leo.modules.tptputils
 
 import leo.datastructures.TPTP
+import leo.datastructures.TPTP.TFF
 
 object ParseTree {
 
@@ -32,7 +33,43 @@ object ParseTree {
   }
 
   private[this] final def thfStatement(thfStatement: TPTP.THF.Statement): String = ""
-  private[this] final def tffStatement(thfStatement: TPTP.TFF.Statement): String = ""
+
+  private[this] final def tffStatement(tffStatement: TPTP.TFF.Statement): String = {
+    tffStatement match {
+      case TFF.Typing(atom, typ) => s"{ type : 'typing', name : '${atom}', body : ${tffType(typ)} }"
+      case TFF.Logical(formula) => tffFormula(formula)
+    }
+  }
+  private[this] final def tffFormula(formula: TPTP.TFF.Formula): String = formula match {
+    case TFF.AtomicFormula(f, args) => s"{ type : 'atomicFormula' , name : '$f' , body : ${args.map(tffTerm).mkString("[", ", ", "]")} }"
+    case TFF.QuantifiedFormula(quantifier, variableList, body) => s"{ type : 'quantifier' , quantifier : '${quantifier.pretty}' , vars : '' , body : [${tffFormula(body)}] }"
+    case TFF.UnaryFormula(connective, body) => s"{ type : 'connective' , connective : '${connective.pretty}' , body : [${tffFormula(body)}] }"
+    case TFF.BinaryFormula(connective, left, right) => s"{ type : 'connective' , connective : '${connective.pretty}' , body : [${tffFormula(left)}, ${tffFormula(right)}] }"
+    case TFF.Equality(left, right) => s"{ type : 'connective' , connective : '=' , body : [${tffTerm(left)}, ${tffTerm(right)}] }"
+    case TFF.Inequality(left, right) => s"{ type : 'connective' , connective : '!=' , body : [${tffTerm(left)}, ${tffTerm(right)}}] }"
+    case TFF.FormulaVariable(name) => s"{ type : 'variable' , name : '$name' }"
+    case TFF.ConditionalFormula(condition, thn, els) => s"{ type : 'connective' , connective : 'ite' , body : [${tffFormula(condition)}, ${tffTerm(thn)}, ${tffTerm(els)}}] }"
+    case TFF.LetFormula(typing, binding, body) => ???
+    case TFF.Assignment(lhs, rhs) => s"{ type : 'connective' , connective : 'assignment' , body : [${tffTerm(lhs)}, ${tffTerm(rhs)}] }"
+    case TFF.MetaIdentity(lhs, rhs) => s"{ type : 'connective' , connective : 'metaEq' , body : [${tffTerm(lhs)}, ${tffTerm(rhs)}] }"
+    case TFF.NonclassicalPolyaryFormula(connective, args) => s"{ type : 'connective' , connective : '' , body : ${args.map(tffFormula).mkString("[", ", ", "]")} }"
+  }
+  private[this] final def tffTerm(term: TPTP.TFF.Term): String = term match {
+    case TFF.AtomicTerm(f, args) => s"{ type : 'atomicTerm' , name : '$f' , body : ${args.map(tffTerm).mkString("[", ", ", "]")} }"
+    case TFF.Variable(name) => s"{ type : 'variable' , name : '$name' }"
+    case TFF.DistinctObject(name) => s"{ type : 'distinct' , name : '$name' }"
+    case TFF.NumberTerm(value) => s"{ type : 'number' , value : '${value.pretty}' }"
+    case TFF.Tuple(elements) => s"{ type : 'tuple' , body : ${elements.map(tffTerm).mkString("[", ", ", "]")} }"
+    case TFF.FormulaTerm(formula) => s"{ type : 'formulaTerm' , body : [${tffFormula(formula)}] }"
+  }
+  private[this] final def tffType(typ: TPTP.TFF.Type): String = typ match {
+    case TFF.AtomicType(name, args) => s"{ type : 'atomicType' , name : '$name' , body : ${args.map(tffType).mkString("[", ", ", "]")} }"
+    case TFF.MappingType(left, right) => s"{ type : 'connective' , connective : '>' , body : [${left.map(tffType).mkString(",")}, ${tffType(right)}] }"
+    case TFF.QuantifiedType(variables, body) => s"{ type : 'quantifier' , quantifier : '!' , vars : ${variables.map(_._1).mkString("[", ", ", "]")} , body : [${tffType(body)}] }"
+    case TFF.TypeVariable(name) => s"{ type : 'variable' , name : '$name' }"
+    case TFF.TupleType(components) => s"{ type : 'tuple' , body : ${components.map(tffType).mkString("[", ", ", "]")} }"
+  }
+
   private[this] final def fofStatement(thfStatement: TPTP.FOF.Statement): String = ""
   private[this] final def tcfStatement(thfStatement: TPTP.TCF.Statement): String = ""
   private[this] final def cnfStatement(thfStatement: TPTP.CNF.Statement): String = ""
